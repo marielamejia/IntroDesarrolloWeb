f5ab08d8e008be28f4ea4571a6e1afb38d724e40
Mariela Mejía <marielamejiagt@gmail.com>
2025-09-19 09:58:23 -0600

    ejercicio javascript

---
+++ ejercicioMultiplicacion/multiplicacion.js
@@ -0,0 +1,15 @@
+//multiplicacion clasica
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+//multiplicacion con arrow
+const mumultiplicacionArrow = (a, b) => a * b;
+
+//cuadrado clasico
+const cuadradoClasico = function(x) {
+    return x * x;
+};
+
+//cuadrado con arrow 
+const cuadradoArrow = x => x * x;
